4b4fdcf4ae1d2dad429277359f9396eca02db663
George Taveras <george.taveras1231@gmail.com>
2016-11-20 21:42:24 -0500

    Add unit tests for resolver

---
+++ test/units/resolver-test.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+var path = require('path');
+
+var resolver = require('../../lib/resolver');
+
+describe('resolver', function () {
+  var fakeRequire = function (_sassImportPath) {
+    // Simulate package.json
+    return { sass: 'index.scss' };
+  };
+
+  fakeRequire.resolve = function (sassImportPath) {
+    return path.join('a', 'b', 'c', sassImportPath);
+  };
+
+  describe('entrypoint import', function () {
+    it('resolves the entrypoint path', function () {
+      var resolve = resolver(fakeRequire);
+
+      var result = resolve('package-name');
+
+      assert.equal(path.join('a', 'b', 'c', 'package-name', 'index.scss'), result);
+    });
+  });
+
+  describe('non entrypoint import', function () {
+    it('resolves the correct path', function () {
+      var resolve = resolver(fakeRequire);
+
+      var result = resolve('package-name/specific-file-in-package');
+
+      assert.equal(path.join('a', 'b', 'c', 'package-name', 'specific-file-in-package'), result);
+    });
+  })
+});
